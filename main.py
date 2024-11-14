607f896acfeb088702dc3cd880c4d7c6e7fcd04e
klaymean <amirhosseinnezamivand@gmail.com>
2024-11-14 16:54:03 +0100

    added  Embedding

--- main.py
+++ main.py
@@ -15,7 +15,22 @@ bot = commands.Bot(command_prefix="#", intents=intents)
 
 
 
+def normalize_query(query):
+    query = query.lower()
+    query = query.replace(" ", "")
+    return query
+
+
+
+
+
+
+
+
 def quote(query):
+
+    query = normalize_query(query)
+
     if query == 'random':
         response = requests.get('https://zenquotes.io/api/random')
         json_data = json.loads(response.text)
@@ -23,12 +38,12 @@ def quote(query):
         quote = f"**{json_data[0]['q']}**\n\n*— {json_data[0]['a']}*"
         return quote
     
+
     elif query == 'stoic2':
          response = requests.get('https://stoic.tekloon.net/stoic-quote')
          json_data = json.loads(response.text)
          #quote = json_data['data']['quote'] + "\r\n - " + json_data['data']['author']
          quote = f"**{json_data['data']['quote']}**\n\n*— {json_data['data']['author']}*"
-
          return quote
     
 
@@ -36,14 +51,25 @@ def quote(query):
          with open('stoic_quotes.json', 'r') as f:
              json_data = json.load(f)
          quote_data = random.choice(json_data['quotes'])
-    
-         # Format the quote and author
          quote = f"**{quote_data['text']}**\n*{quote_data['author']}*"
          
+         image_folder = 'images/stoic'
+         valid_images = [img for img in os.listdir(image_folder) if img.endswith(('.png', '.jpeg', '.jpg'))]
 
+         image_file = random.choice(valid_images)
+         image_path = os.path.join(image_folder, image_file)
 
+         embed = discord.Embed(
+            title="🧘 Stoic Pand",
+            description=f"\"{quote}\"",
+            color=discord.Color.purple()
+         )
 
-         return quote
+         file = discord.File(image_path, filename=image_file)
+         embed.set_image(url=f"attachment://{image_file}")
+         return embed, file
+             
+         
 
 
 
@@ -51,27 +77,120 @@ def quote(query):
          with open('programming_quotes.json','r') as f:
             json_data = json.load(f)
          random_quote = random.choice(json_data)
-         #quote = random_quote['en'] + "\r\n - " + random_quote['author']
          quote = f"**{random_quote['en']}**\n\n*— {random_quote['author']}*"
+         
+         image_folder = 'images/programming'
+         valid_images = [img for img in os.listdir(image_folder) if img.endswith(('.png', '.jpeg', '.jpg'))]
 
-         return quote
+         image_file = random.choice(valid_images)
+         image_path = os.path.join(image_folder, image_file)
+
+         embed = discord.Embed(
+            title="💻 Programming Pand",
+            description=f"\"{quote}\"",
+            color=discord.Color.blue()
+         )
+
+         file = discord.File(image_path, filename=image_file)
+         embed.set_image(url=f"attachment://{image_file}")
+         return embed, file        
     
-    elif query in ['suntzu', 'sun tzu','Sun Tzu']:
+
+    elif query == 'suntzu':
          with open('suntzu_quotes.json','r') as f:
             json_data = json.load(f)
          #quote = random.choice(json_data) + '\r\n-' 'Sun Tzu'
          quote = f"**{random.choice(json_data)}**\n\n*— Sun Tzu*"
-         return quote
+         
+         image_folder = 'images/suntzu'
+         valid_images = [img for img in os.listdir(image_folder) if img.endswith(('.png', '.jpeg', '.jpg'))]
     
-    elif query in ['topg']:
+         image_file = random.choice(valid_images)
+         image_path = os.path.join(image_folder, image_file)
+
+         embed = discord.Embed(
+            title="📚 Sun Tzu Pand",
+            description=f"\"{quote}\"",
+            color=discord.Color.orange()
+         )
+
+         file = discord.File(image_path, filename=image_file)
+         embed.set_image(url=f"attachment://{image_file}")
+         return embed, file
+         
+    
+
+    elif query == 'topg':
         with open('topg.json','r') as f:
             json_data = json.load(f)
         quote = f"**{random.choice(json_data)}**\n\n*— Andrew Tate*"
-        return quote
-    elif query in ['stallman']:
+        
+        image_folder = 'images/topg'
+        valid_images = [img for img in os.listdir(image_folder) if img.endswith(('.png', '.jpeg', '.jpg'))]
+
+        image_file = random.choice(valid_images)
+        image_path = os.path.join(image_folder, image_file)
+
+        embed = discord.Embed(
+            title="🥋 Andrew Tate Pand",
+            description=f"\"{quote}\"",
+            color=discord.Color.red()
+        )
+
+        file = discord.File(image_path, filename=image_file)
+        embed.set_image(url=f"attachment://{image_file}")
+        return embed, file
+    
+
+
+    elif query == 'stallman':
         with open('stallman.json','r') as f:
             json_data = json.load(f)
         quote = f"**{random.choice(json_data)}**\n\n*— Richard Stallman*"
+
+        image_folder = 'images/stallman'
+        valid_images = [img for img in os.listdir(image_folder) if img.endswith(('.png', '.jpeg', '.jpg'))]
+
+        image_file = random.choice(valid_images)
+        image_path = os.path.join(image_folder, image_file)
+
+        embed = discord.Embed(
+            title="🐧 Richard Stallman Pand",
+            description=f"\"{quote}\"",
+            color=discord.Color.blue()
+        )
+
+        file = discord.File(image_path, filename=image_file)
+        embed.set_image(url=f"attachment://{image_file}")
+        return embed, file 
+    
+    elif query == 'mc':
+        with open('mc.json','r') as f:
+            json_data = json.load(f)
+        quote = f"**{random.choice(json_data)}**\n\n*— Minecraft*"
+        #return quote
+        image_folder = 'images/mc'
+        valid_images = [img for img in os.listdir(image_folder) if img.endswith(('.png', '.jpeg', '.jpg'))]
+
+        image_file = random.choice(valid_images)
+        image_path = os.path.join(image_folder, image_file)
+
+        embed = discord.Embed(
+            title="💬 Minecraft Pand",
+            description=f"\"{quote}\"",
+            color=discord.Color.green()
+        )
+        #embed.set_footer(text="— Minecraft 🧊")
+
+        file = discord.File(image_path, filename=image_file)
+        embed.set_image(url=f"attachment://{image_file}")
+        
+        return embed, file
+
+    
+
+
+
          
         
 
@@ -85,17 +204,26 @@ async def on_ready():
 
 
 
-
 @bot.command()
 async def pand(ctx, query: str):
     
-    await ctx.send(quote(query))
+    response = quote(query)
+    
+    if isinstance(response, tuple):
+        embed, file = response
+        await ctx.send(embed=embed, file=file)
+    
+    elif isinstance(response, str):
+        await ctx.send(response)
+
+    else:
+        await ctx.send("Bebakshid, chi?")
 
 
 
 
-@bot.command(help="Listi az pand haye mojood")
-async def help(ctx, query: str):
+@bot.command()
+async def komak(ctx):
     help_messages = """
     **Pand haye mojood:**
     - 'random': - In ke maloome chie khob ahmaq
@@ -105,14 +233,12 @@ async def help(ctx, query: str):
     - 'suntzu': - Sun Tzu pands
     - 'topg': - Andrew Tate pands
     - 'stallman': - Richard Stallman pands
+    - 'mc': - Minecraft pands
 
     """
     await ctx.send(help_messages)
 
 
-
-#with open('config.json','r') as f:
- #    config = json.load(f)
 
 
 TOKEN = os.getenv("TOKEN")
